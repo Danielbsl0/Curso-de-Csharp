592f0ee1a032adfc2f1cc7f1857a101b621d8509
Daniel <barbosadaniel893@gmail.com>
2025-07-10 16:38:17 -0300

    Fazendo a lista de For da matéria

---
+++ listaFor_2/Program.cs
@@ -0,0 +1,15 @@
+﻿string palavra;
+int cont = 0;
+
+Console.WriteLine("Digite uma palavra: ");
+palavra = Console.ReadLine();
+
+for (int i = 1; i < palavra.Length; i++) { 
+    if (palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i' ||
+        palavra[i] == 'o' || palavra[i] == 'u')
+    {
+        cont++;
+    }
+}
+
+Console.WriteLine($"A quantidade de vogais na palavra é: {cont}");
