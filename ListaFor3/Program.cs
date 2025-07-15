d7fb7bfbbef82cd9796f9fb9dd62084fe15db9b3
Daniel <barbosadaniel893@gmail.com>
2025-07-15 17:31:02 -0300

    Finalizando lista 3

--- ListaFor3/Program.cs
+++ ListaFor3/Program.cs
@@ -1,36 +1,22 @@
 ﻿int num;
 string bin = "";
 bool isValid;
-
 do
 {
     Console.Write("Insira um número inteiro: ");
     isValid = int.TryParse(Console.ReadLine(), out num);
 
     if (isValid)
-    {        
+    {
         if (num < 0)
         {
             Console.WriteLine("Por favor, insira um número inteiro positivo.");
             isValid = false;
             continue;
-        }else if (num == 0)
+        }
+        else if (num == 0)
         {
             bin = "0";
-        }
-        else
-        {
-            for (int i = num; i >= 1; i /= 2)
-            {
-                if (i > 1)
-                {
-                    bin = Convert.ToString(i % 2) + bin;
-                }
-                else
-                {
-                    bin = Convert.ToString(i) + bin;
-                }
-            }
         }
 
     }
@@ -38,6 +24,17 @@ do
     {
         Console.WriteLine("Entrada inválida, por favor insira um valor válido");
     }
-}while (!isValid);
+} while (!isValid);
+for (int i = num; i >= 1; i /= 2)
+{
+    if (i > 1)
+    {
+        bin = Convert.ToString(i % 2) + bin;
+    }
+    else
+    {
+        bin = Convert.ToString(i) + bin;
+    }
+}
 
 Console.WriteLine(bin);
